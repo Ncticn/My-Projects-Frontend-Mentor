60a22d18a55c1a2bd0c896b0799e84ffc650e8f8
Necati CAN <necati.can@ottoo.com>
2024-08-25 08:33:06 +0300

    Single

---
+++ vue-project/src/components/Card/WhyUsCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="why-us">
+    <h3 class="why-us-title color-white">{{ title }}</h3>
+    <p class="why-us-description color-light-gray">{{ description }}</p>
+  </div>
+</template>
---
+++ vue-project/src/main.js
@@ -0,0 +1,6 @@
+import "./assets/Scss/style.css"
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
