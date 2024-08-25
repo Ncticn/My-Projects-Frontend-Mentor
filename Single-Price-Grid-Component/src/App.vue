7e71e3d63c64d43395ce2601756c87e5525adacd
Necati CAN <necati.can@ottoo.com>
2024-08-25 08:38:26 +0300

    Folder Name Edited

---
+++ Single-Price-Grid-Component/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SinglePriceGrid from "@/components/Card/SinglePriceGrid.vue";
+import FooterSign from "@/components/Footer/FooterSign.vue";
+</script>
+
+<template>
+  <main>
+    <SinglePriceGrid />
+  </main>
+  <footer>
+    <FooterSign/>
+  </footer>
+</template>
