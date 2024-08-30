904e5c21194dee67406c0ebe4ad47ff4a68519a2
Necati Can <nctiicn@gmail.com>
2024-08-30 08:18:22 +0300

    Added Custom Settings to TailwindCss

--- Ping-Single-Column-Coming-Soon-Page/tailwind.config.js
+++ Ping-Single-Column-Coming-Soon-Page/tailwind.config.js
@@ -4,8 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
+  theme: { // Varsayılanları değiştirmek için kullanılır.
+    colors:{
+      'blue': 'hsl(223, 87%, 63%)',
+      'gray': 'hsl(0, 0%, 59%)'
+    },
+    fontFamily: {
+      sans: ["Libre Franklin", 'sans-serif']
+    },
+    extend: { // Yeni bir class eklemek için kullanılır.
+      colors: {
+        'pale-blue' : 'hsl(223, 100%, 88%)',
+        'light-red': 'hsl(354, 100%, 66%)',
+        'very-dark-blue': 'hsl(209, 33%, 12%)',
+      }
+    }, 
   },
   plugins: [],
 }
